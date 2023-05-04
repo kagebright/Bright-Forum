14428ad0c073ca4fcf0b3d5c79e97da21fff1272
Bright <113262558+kagebright@users.noreply.github.com>
2023-05-04 14:56:18 -0400

    new updates

--- client/src/components/Replies.js
+++ client/src/components/Replies.js
@@ -1,31 +1,60 @@
 import React, { useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 
 const Replies = () => {
+    const [replyList, setReplyList] = useState([]);
     const [reply, setReply] = useState("");
+    const [title, setTitle] = useState("");
+    const navigate = useNavigate();
+    const { id } = useParams();
 
+    useEffect(() => {
+        const fetchReplies = () => {
+            fetch("http://localhost:4000/api/thread/replies", {
+                method: "POST",
+                body: JSON.stringify({
+                    id,
+                }),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            })
+                .then((res) => res.json())
+                .then((data) => {
+                    setReplyList(data.replies);
+                    setTitle(data.title);
+                })
+                .catch((err) => console.error(err));
+        };
+        fetchReplies();
+    }, [id]);
+
+    //👇🏻 This function when triggered when we add a new reply
     const handleSubmitReply = (e) => {
         e.preventDefault();
         console.log({ reply });
         setReply("");
     };
+    return <main className='replies'>{/*--UI elements--*/}</main>;
 
-    return (
-        <main className='replies'>
-            <form className='modal__content' onSubmit={handleSubmitReply}>
-                <label htmlFor='reply'>Reply to the thread</label>
-                <textarea
-                    rows={5}
-                    value={reply}
-                    onChange={(e) => setReply(e.target.value)}
-                    type='text'
-                    name='reply'
-                    className='modalInput'
-                />
 
-                <button className='modalBtn'>SEND</button>
-            </form>
-        </main>
-    );
+    // return (
+    //     <main className='replies'>
+    //         <form className='modal__content' onSubmit={handleSubmitReply}>
+    //             <label htmlFor='reply'>Reply to the thread</label>
+    //             <textarea
+    //                 rows={5}
+    //                 value={reply}
+    //                 onChange={(e) => setReply(e.target.value)}
+    //                 type='text'
+    //                 name='reply'
+    //                 className='modalInput'
+    //             />
+
+    //             <button className='modalBtn'>SEND</button>
+    //         </form>
+    //     </main>
+    // );
 };
 
 export default Replies;
--- server/server.js
+++ server/server.js
@@ -13,12 +13,72 @@ const users = [];
 //👇🏻 generates a random string as ID
 const generateID = () => Math.random().toString(36).substring(2, 10);
 
+app.get("/api/all/threads", (req, res) => {
+    res.json({
+        threads: threadList,
+    });
+});
+
 app.post("/api/register", async (req, res) => {
     const { email, password, username } = req.body;
     //👇🏻 holds the ID
     const id = generateID();
     //👇🏻 logs all the user's credentials to the console.
     console.log({ email, password, username, id });
+});
+
+app.post("/api/thread/like", (req, res) => {
+    //👇🏻 accepts the post id and the user id
+    const { threadId, userId } = req.body;
+    //👇🏻 gets the reacted post
+    const result = threadList.filter((thread) => thread.id === threadId);
+    //👇🏻 gets the likes property
+    const threadLikes = result[0].likes;
+    //👇🏻 authenticates the reaction
+    const authenticateReaction = threadLikes.filter((user) => user === userId);
+    //👇🏻 adds the users to the likes array
+    if (authenticateReaction.length === 0) {
+        threadLikes.push(userId);
+        return res.json({
+            message: "You've reacted to the post!",
+        });
+    }
+    //👇🏻 Returns an error user has reacted to the post earlier
+    res.json({
+        error_message: "You can only react once!",
+    });
+});app.post("/api/thread/like", (req, res) => {
+    //👇🏻 accepts the post id and the user id
+    const { threadId, userId } = req.body;
+    //👇🏻 gets the reacted post
+    const result = threadList.filter((thread) => thread.id === threadId);
+    //👇🏻 gets the likes property
+    const threadLikes = result[0].likes;
+    //👇🏻 authenticates the reaction
+    const authenticateReaction = threadLikes.filter((user) => user === userId);
+    //👇🏻 adds the users to the likes array
+    if (authenticateReaction.length === 0) {
+        threadLikes.push(userId);
+        return res.json({
+            message: "You've reacted to the post!",
+        });
+    }
+    //👇🏻 Returns an error user has reacted to the post earlier
+    res.json({
+        error_message: "You can only react once!",
+    });
+});
+
+app.post("/api/thread/replies", (req, res) => {
+    //👇🏻 The post ID
+    const { id } = req.body;
+    //👇🏻 searches for the post
+    const result = threadList.filter((thread) => thread.id === id);
+    //👇🏻 return the title and replies
+    res.json({
+        replies: result[0].replies,
+        title: result[0].title,
+    });
 });
 
 app.listen(PORT, () => {
